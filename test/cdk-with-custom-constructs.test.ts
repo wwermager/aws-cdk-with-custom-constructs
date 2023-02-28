7265f6ef8cfe0f8d0b4ba4d2f6ea22d0e85a9d17
Will Wermager <wwermager@Wills-MacBook-Pro.local>
2023-02-28 13:31:06 -0600

    Initial commit

---
+++ test/cdk-with-custom-constructs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkWithCustomConstructs from '../lib/cdk-with-custom-constructs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-with-custom-constructs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkWithCustomConstructs.CdkWithCustomConstructsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
